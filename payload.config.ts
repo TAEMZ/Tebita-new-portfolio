bb5ef4c15c25ef6e14776b39e7bbd2dbc4317180
taemz <abiykibru6@gmail.com>
2025-12-31 20:49:43 +0300

    edit payload config

--- payload.config.ts
+++ payload.config.ts
@@ -159,7 +159,7 @@ export default buildConfig({
     },
     db: postgresAdapter({
         pool: {
-            connectionString: process.env.DATABASE_URI || '',
+            connectionString: process.env.DATABASE_URI || process.env.DATABASE_URL || '',
             ssl: {
                 rejectUnauthorized: false,
             },
